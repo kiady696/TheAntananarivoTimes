ecb61eaac459307c8881a42947fe412519399e6c
kix696 <Kiadyrajwels.dark@gmail.com>
2020-08-02 22:57:12 +0200

    enleve les accents a linput du titre

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'mysql-kiadyad.alwaysdata.net',
-	'username' => 'kiadyad',
-	'password' => 'Skrillex218',
-	'database' => 'kiadyad_antananarivo_times',
+	'hostname' => 'localhost', 
+	'username' => 'root',
+	'password' => 'root',
+	'database' => 'news',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
