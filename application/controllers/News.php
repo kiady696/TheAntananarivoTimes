3cbdeee2e7bd85b800f9895fa1f670430470b4bd
kix696 <Kiadyrajwels.dark@gmail.com>
2020-08-02 21:41:00 +0200

    Addeda form for entering news

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -8,6 +8,30 @@ class News extends CI_Controller { //default controller mapiseo ny news rehetra
                 $this->load->helper('url_helper');
         }
 
+        public function create()
+{
+    $this->load->helper('form');
+    $this->load->library('form_validation');
+
+    $data['title'] = 'Entrez votre nouvel article';
+
+    $this->form_validation->set_rules('title', 'Title', 'required');
+    $this->form_validation->set_rules('text', 'Text', 'required');
+
+    if ($this->form_validation->run() === FALSE)  //ra misy diso ny inputs
+    {
+        $this->load->view('templates/header', $data);
+        $this->load->view('news/create');
+        $this->load->view('templates/footer');
+
+    }
+    else
+    {
+        $this->news_model->set_news();
+        $this->load->view('news/success');
+    }
+}
+
         public function index()
         {
                 $data['news'] = $this->news_model->get_news();
@@ -38,4 +62,6 @@ class News extends CI_Controller { //default controller mapiseo ny news rehetra
         $this->load->view('news/view', $data);
         $this->load->view('templates/footer');
         }
+
+        
 }
---
+++ application/views/news/create.php
@@ -0,0 +1,16 @@
+<h2><?php echo $title; ?></h2>
+
+
+<?php echo form_open('news/create'); ?>
+
+    <label for="title">Titre</label>
+    <input type="text" name="title" /><br />
+
+    <label for="text">Text</label>
+    <textarea name="text"></textarea><br />
+
+    <input type="submit" name="submit" value="Ajouter votre article" />
+
+</form>
+
+<?php echo validation_errors(); ?>
