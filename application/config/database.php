241f76940f2afd7c020277ed655648b0a58dde99
kix696 <Kiadyrajwels.dark@gmail.com>
2020-08-03 11:28:42 +0200

    bola miova le config ao am gitignore

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost', 
+	'hostname' => 'mysql-kiadyad.alwaysdata.net', 
 	'username' => 'kiadyad',
 	'password' => 'Skrillex218',
 	'database' => 'kiadyad_antananarivo_times',
